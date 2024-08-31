a24aabe66720b1cbc45ba4365d9c31606c6b1dcd
ramees7 <144250876+ramees7@users.noreply.github.com>
2024-09-01 01:06:03 +0530

    some small mistake cleared

--- src/pages/BookDemo.jsx
+++ src/pages/BookDemo.jsx
@@ -89,57 +89,57 @@ export default function BookDemo() {
         <h1 className="font-bold text-4xl text-center mb-7 text-[#000080]">
           Book Free Demo
         </h1>
-        <form class="max-w-md mx-auto" onSubmit={handleSubmit}>
-          <div class="relative z-0 w-full mb-6 group">
+        <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+          <div className="relative z-0 w-full mb-6 group">
             <input
               type="text"
               name="name"
               id="name"
-              class="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
+              className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
               placeholder=" "
               value={formData.name}
               onChange={handleChange}
               required
             />
             <label
-              for="name"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#000080] peer-focus:dark:text-[#000080] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#000080] peer-focus:dark:text-[#000080] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Name
             </label>
           </div>
-          <div class="relative z-0 w-full mb-6 group">
+          <div className="relative z-0 w-full mb-6 group">
             <input
               type="email"
               name="email"
               id="email"
-              class="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
+              className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
               placeholder=""
               value={formData.email}
               onChange={handleChange}
               required
             />
             <label
-              for="email"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#000080] peer-focus:dark:text-[#000080] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#000080] peer-focus:dark:text-[#000080] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Email address
             </label>
           </div>
-          <div class="relative z-0 w-full mb-6 group">
+          <div className="relative z-0 w-full mb-6 group">
             <input
               type="number"
               name="number"
               id="number"
-              class="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
+              className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
               placeholder=""
               value={formData.number}
               onChange={handleChange}
               required
             />
             <label
-              for="number"
-              class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#000080] peer-focus:dark:text-[#000080] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              htmlFor="number"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#000080] peer-focus:dark:text-[#000080] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Phone Number
             </label>
@@ -150,7 +150,7 @@ export default function BookDemo() {
               name="course"
               value={formData.course}
               onChange={handleChange}
-              class="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
+              className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#000080] focus:outline-none focus:ring-0 focus:border-[#000080] peer"
             >
               <option value="" disabled>
                 Choose an option
@@ -182,7 +182,7 @@ export default function BookDemo() {
           <div className="w-full justify-center">
             <button
               type="submit"
-              class="text-white bg-[#000080] w-full  focus:ring-4 focus:outline-none font-medium rounded-lg text-sm  px-5 py-2.5 text-center mt-5 "
+              className="text-white bg-[#000080] w-full  focus:ring-4 focus:outline-none font-medium rounded-lg text-sm  px-5 py-2.5 text-center mt-5 "
             >
               Send Now
             </button>
